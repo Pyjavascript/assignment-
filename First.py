bdede3bbd6488873c1686039b2a0599a4a49257c
Pyjavascript <102342706+Pyjavascript@users.noreply.github.com>
2022-08-04 23:33:53 +0530

    Create First.py

---
+++ First.py
@@ -0,0 +1,23 @@
+inp = int(input("enter a number: "))
+
+if inp>0 and inp<=60:
+
+    div = inp//3
+
+    a = 0
+
+    b = 0
+
+    c = 0
+
+    a = a+div
+
+    b = b+div
+
+    c = c+div
+
+    print(a,b,c)
+
+else:
+
+    print("Your input should be between 1 and 60")
